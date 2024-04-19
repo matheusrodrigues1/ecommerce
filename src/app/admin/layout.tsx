2044d4272e9548820f77cbb81475bd6ddfe2ed19
matheusrodrigues1 <matech00@outlook.com>
2024-04-18 22:45:43 -0300

    nextauth + google

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,22 @@
+"use client"
+import Login from '@/components/admin-panel/Login'
+import { useAppSelector } from '@/redux/hooks'
+import { useSession } from 'next-auth/react'
+import React from 'react'
+
+const Layout = () => {
+  const isLoading = useAppSelector(store => store.loadingReducer)
+  const {data: session} = useSession()
+
+  if(!session?.user){
+    return <Login/>
+  }
+  
+  return (
+    <div>
+      layout
+    </div>
+  )
+}
+
+export default Layout
